ec63092909c80fc913a42df0096a1999d38460a5
SAISRAVAN <oletisravan@gmail.com>
2022-10-03 15:41:09 +0530

    Sravan:adding custom event listener

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+//
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉Holly ! Molly ! Correct Number !🎊';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+//this is used to help read 
+
+document.querySelector('.check').addEventListener
+   ('click', function () {
+      console.log(document.querySelector('.guess').value);
+   });
